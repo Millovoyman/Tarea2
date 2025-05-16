85c781f2674045feadbcb20809f12063822ddc76
Millovoyman <millovichfm@gmail.com>
2025-05-16 00:00:55 -0600

    Create Ejercicio12.c

---
+++ Ejercicio12.c
@@ -0,0 +1,22 @@
+//Flores Mejía
+//Ejercicio12
+//producto de los N primeros números naturales.
+
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    long long producto = 1;
+
+    printf("Ingrese el valor de N (número de números naturales a multiplicar): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        producto *= i;
+    }
+
+    printf("El producto de los primeros %d números naturales es: %lld\n", n, producto);
+
+    return 0;
+}
