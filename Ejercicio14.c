a24d6f248fb0c1d5623cb18082774c7c6abd0e27
Millovoyman <millovichfm@gmail.com>
2025-05-16 00:10:15 -0600

    Create Ejercicio14.c

---
+++ Ejercicio14.c
@@ -0,0 +1,34 @@
+//Flores Mejía
+//Ejercicio14
+// cuántas veces se encuentra un número dentro del arreglo.
+
+#include <stdio.h>
+
+int main() {
+
+    int tamaño, numero_a_buscar, contador = 0;
+
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &tamaño);
+
+    int arreglo[tamaño];
+
+    printf("Ingrese los elementos del arreglo:\n");
+    for (int i = 0; i < tamaño; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &arreglo[i]);
+    }
+
+    printf("Ingrese el número que desea buscar en el arreglo: ");
+    scanf("%d", &numero_a_buscar);
+
+    for (int i = 0; i < tamaño; i++) {
+        if (arreglo[i] == numero_a_buscar) {
+            contador++;
+        }
+    }
+
+    printf("El número %d aparece %d veces en el arreglo.\n", numero_a_buscar, contador);
+
+    return 0;
+}
