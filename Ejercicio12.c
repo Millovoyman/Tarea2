897c08a1597bd62909f8ca471c060f2c7251edac
Millovoyman <millovichfm@gmail.com>
2025-05-15 23:52:29 -0600

    Create Ejercicio12.c

---
+++ Ejercicio12.c
@@ -0,0 +1,29 @@
+//Flores Mejía
+//Ejercicio12
+//números positivos, negativos o núlos
+
+#include <stdio.h>
+
+int main() {
+
+    int numero, positivos = 0, negativos = 0, nulos = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        printf("Ingrese el número %d: ", i);
+        scanf("%d", &numero);
+
+        if (numero > 0) {
+            positivos++;
+        } else if (numero < 0) {
+            negativos++;
+        } else {
+            nulos++;
+        }
+    }
+
+    printf("Cantidad de números positivos: %d\n", positivos);
+    printf("Cantidad de números negativos: %d\n", negativos);
+    printf("Cantidad de números nulos: %d\n", nulos);
+
+    return 0;
+}
